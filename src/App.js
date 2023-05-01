cec92cb76840efffca5b5b338ad9cffecef080d1
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2023-05-01 16:20:43 +0530

    Project: Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import homeMain from "./assets/homeMain.jpg";
+import Header from './components/Header';
+import PageHeader from './components/PageHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <PageHeader image={homeMain} title='Luxurious Rooms' message='Deluxe Rooms Starting At $299' buttonTitle='Our Rooms'/>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from '../assets/logo.svg'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg bg-light sticky-top">
+                <div class="container">
+                    <Link class="navbar-brand" to="/">
+                        <img src={Logo} alt="Beach Resort" />
+                    </Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse px-5" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item px-2">
+                                <Link class="nav-link text-dark fw-semibold" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li class="nav-item px-2">
+                                <Link class="nav-link text-dark fw-semibold" to="/Rooms">Rooms</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
