6fa5f1b5e5f023454a175002acae994d9ba6d04f
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2023-05-02 14:31:30 +0530

    Updated

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,45 +1,14 @@
 import React from 'react'
 import RoomMain from '../assets/roomMain.jpeg'
 import PageHeader from './PageHeader'
-import FeaturedRoomsCard from './FeaturedRoomsCard'
-import singleEconomy from '../assets/rooms/singleeconomy.jpeg'
-import singleBasic from '../assets/rooms/singlebasic.jpeg'
-import singleStandard from '../assets/rooms/singlestandard.jpeg'
-import singleDeluxe from '../assets/rooms/singledeluxe.jpeg'
-import doubleEconomy from '../assets/rooms/doubleeconomy.jpeg'
-import doubleBasic from '../assets/rooms/doublebasic.jpeg'
-import doubleStandard from '../assets/rooms/doublestandard.jpeg'
-import doubleDeluxe from '../assets/rooms/doubledeluxe.jpeg'
-import familyEconomy from '../assets/rooms/familyeconomy.jpeg'
-import familyBasic from '../assets/rooms/familybasic.jpeg'
-import familyStandard from '../assets/rooms/familystandard.jpeg'
-import familyDeluxe from '../assets/rooms/familydeluxe.jpeg'
-import presidential from '../assets/rooms/presidential.jpeg'
+import SearchRoomSection from './SearchRoomSection'
 
 
 export default function Room() {
   return (
     <>
         <PageHeader title='Our Rooms' image={RoomMain} buttonTitle='Return Home'/>
-        <div class="container d-flex p-5 flex-wrap"  id="cards">
-        <FeaturedRoomsCard width="22%" message="Single Economy" price="$ 100" image={singleEconomy}/>
-            <FeaturedRoomsCard width="22%" message="Single Basic" price="$ 150" image={singleBasic}/>
-            <FeaturedRoomsCard width="22%" message="Single Standard" price="$ 250" image={singleStandard}/>
-            <FeaturedRoomsCard width="22%" message="Single Deluxe" price="$ 300" image={singleDeluxe}/>
-            <FeaturedRoomsCard width="22%" message="Double Economy" price="$ 200" image={doubleEconomy}/>
-            <FeaturedRoomsCard width="22%" message="Double Basic" price="$ 250" image={doubleBasic}/>
-            <FeaturedRoomsCard width="22%" message="Double Standard" price="$ 300" image={doubleStandard}/>
-            <FeaturedRoomsCard width="22%" message="Double Deluxe" price="$ 400" image={doubleDeluxe}/>
-            <FeaturedRoomsCard width="22%" message="Family Economy" price="$ 300" image={familyEconomy}/>
-            <FeaturedRoomsCard width="22%" message="Family Basic" price="$ 350" image={familyBasic}/>
-            <FeaturedRoomsCard width="22%" message="Family Standard" price="$ 400" image={familyStandard}/>
-            <FeaturedRoomsCard width="22%" message="Family Deluxe" price="$ 500" image={familyDeluxe}/>
-            <FeaturedRoomsCard width="22%" message="Presidential" price="$ 600" image={presidential}/>
-            </div>
-        <div className='container d-flex justify-content-around p-5'>
-           
-         
-        </div>  
+        <SearchRoomSection />
     </>
   )
 }
--- src/components/SearchRoomSection.js
+++ src/components/SearchRoomSection.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import '../components/roomsearch.css'
+import '../assets/stylesheets/roomsearch.css'
+
+import FeaturedRoomsCard from './FeaturedRoomsCard'
+import singleEconomy from '../assets/rooms/singleeconomy.jpeg'
+import singleBasic from '../assets/rooms/singlebasic.jpeg'
+import singleStandard from '../assets/rooms/singlestandard.jpeg'
+import singleDeluxe from '../assets/rooms/singledeluxe.jpeg'
+import doubleEconomy from '../assets/rooms/doubleeconomy.jpeg'
+import doubleBasic from '../assets/rooms/doublebasic.jpeg'
+import doubleStandard from '../assets/rooms/doublestandard.jpeg'
+import doubleDeluxe from '../assets/rooms/doubledeluxe.jpeg'
+import familyEconomy from '../assets/rooms/familyeconomy.jpeg'
+import familyBasic from '../assets/rooms/familybasic.jpeg'
+import familyStandard from '../assets/rooms/familystandard.jpeg'
+import familyDeluxe from '../assets/rooms/familydeluxe.jpeg'
+import presidential from '../assets/rooms/presidential.jpeg'
 
 const SearchRoomSection = () => {
     let main_text = {
@@ -11,16 +26,13 @@ const SearchRoomSection = () => {
         letterSpacing: "3px",
     }
     return (
-        <>
-            <div className='container text-center' style={main_text}>
-                Search Room
-            </div>
-            <div className="container" id='line_under_text'>
-            </div>
-            <div className="container d-flex flex-row all_items">
+        <div className='container-flex'>
+            <h1 className='text-center pt-5 pb-2' style={main_text}>Search Room</h1>
+            <div className="container" id='line_under_text' />
+            <div className="container d-flex flex-row justify-content-around flex-wrap align-items-center p-5">
                 <div className='select-option'>
-                    <label htmlFor="roomtype">Rooms Type</label><br></br>
-                    <select className="custom-select custom-select-lg mb-5" id='select_room_type'>
+                    <label className="py-3" htmlFor="roomtype">Rooms Type</label><br></br>
+                    <select className="custom-select custom-select-lg" id='select_room_type'>
                         <option value="All">All</option>
                         <option value="Single">Single</option>
                         <option value="Double">Double</option>
@@ -30,8 +42,8 @@ const SearchRoomSection = () => {
                 </div>
 
                 <div className='select-option2'>
-                    <label htmlFor="geust">Guests</label><br></br>
-                    <select className="custom-select custom-select-lg mb-5" id='select_guest_number'>
+                    <label  className="py-3" htmlFor="guest">Guests</label><br></br>
+                    <select className="custom-select custom-select-lg" id='select_guest_number'>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -43,24 +55,24 @@ const SearchRoomSection = () => {
                     </select>
                 </div>
                 <div className="form-group range_select">
-                    <label htmlFor="ControlRange" id='range_id'>Room Price  $200</label><br></br>
-                    <input type="range"  className="form-control-range" id="controlrange" />
+                    <label className="py-3" htmlFor="ControlRange" id='range_id'>Room Price  $200</label><br />
+                    <input type="range" className="form-control-range" id="controlrange" />
                 </div>
 
                 <div className="form-group text-start">
-                    <label htmlFor="formControlRange">Room Size</label><br></br>
+                    <label className="py-3" htmlFor="formControlRange">Room Size</label><br></br>
                     <input className="form-input" id='min_size' type="number" min="0" defaultValue={"0"} max="1000" />
                     <input className="form-input" id='max_size' type="number" min="0" max="1000" defaultValue={"1000"} />
                 </div>
 
-                <div className='conatiner ' id='last_option'>
-                    <div className="form-check">
+                <div id='last_option'>
+                    <div className="form-check pt-3">
                         <input className="form-check-input" type="checkbox" value="BreakFast" id="BreakFast" />
                         <label className="form-check-label" >
                             BreakFast
                         </label>
                     </div>
-                    <div className="form-check">
+                    <div className="form-check pb-2">
                         <input className="form-check-input" type="checkbox" value="Pets" id="Pets" />
                         <label className="form-check-label">
                             Pets
@@ -68,7 +80,23 @@ const SearchRoomSection = () => {
                     </div>
                 </div>
             </div>
-        </>
+
+            <div class="container d-flex p-5 flex-wrap" id="cards">
+                <FeaturedRoomsCard width="290px" message="Single Economy" price="$ 100" image={singleEconomy} />
+                <FeaturedRoomsCard width="290px" message="Single Basic" price="$ 150" image={singleBasic} />
+                <FeaturedRoomsCard width="290px" message="Single Standard" price="$ 250" image={singleStandard} />
+                <FeaturedRoomsCard width="290px" message="Single Deluxe" price="$ 300" image={singleDeluxe} />
+                <FeaturedRoomsCard width="290px" message="Double Economy" price="$ 200" image={doubleEconomy} />
+                <FeaturedRoomsCard width="290px" message="Double Basic" price="$ 250" image={doubleBasic} />
+                <FeaturedRoomsCard width="290px" message="Double Standard" price="$ 300" image={doubleStandard} />
+                <FeaturedRoomsCard width="290px" message="Double Deluxe" price="$ 400" image={doubleDeluxe} />
+                <FeaturedRoomsCard width="290px" message="Family Economy" price="$ 300" image={familyEconomy} />
+                <FeaturedRoomsCard width="290px" message="Family Basic" price="$ 350" image={familyBasic} />
+                <FeaturedRoomsCard width="290px" message="Family Standard" price="$ 400" image={familyStandard} />
+                <FeaturedRoomsCard width="290px" message="Family Deluxe" price="$ 500" image={familyDeluxe} />
+                <FeaturedRoomsCard width="290px" message="Presidential" price="$ 600" image={presidential} />
+            </div>
+        </div>
     )
 }
 export default SearchRoomSection
